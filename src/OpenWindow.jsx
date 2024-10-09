c4981e3279430bde93e7f912fce72f568c83563a
66sqream <fxckhxtrxd@gmail.com>
2024-10-10 03:13:44 +0400

    ok

--- src/OpenWindow.jsx
+++ src/OpenWindow.jsx
@@ -5,7 +5,7 @@ export const OpenWindow = ({ title, description, onClose, onPostpone }) => {
     <div className="openWindow">
       <h2>{title}</h2>
       <p>{description}</p>
-      <button onClick={onClose}>Закрыть</button>
+      <button onClick={onClose}>Ок</button>
       <button onClick={onPostpone}>Отложить</button>
     </div>
   );
